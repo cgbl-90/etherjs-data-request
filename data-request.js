bca8e6a31615152e88c1493d683803eea4deb8e8
carmen <58992828+cgbl-90@users.noreply.github.com>
2022-11-29 14:43:58 +0100

    Update data-request.js

--- data-request.js
+++ data-request.js
@@ -4,9 +4,26 @@ const provider = new ethers.providers.JsonRpcProvider(
   `https://mainnet.infura.io/v3/${INFURA_ID}`
 );
 
-const address = "0xd24400ae8bfebb18ca49be86258a3c749cf46853"; // GEMINI contract
+/*
+
+The ABI, Application Binary Interface, is basically how you call functions in a contract and get data back.
+
+An ABI determines such details as how functions are called and in which binary format information should be passed from one program component to the next...
+
+An Ethereum smart contract is bytecode deployed on the Ethereum blockchain. There could be several functions in a contract. An ABI is necessary so that you can specify which function in the contract to invoke, as well as get a guarantee that the function will return data in the format you are expecting.
+
+From Ethereum's ABI specification, an example:
+
+contract Foo {
+  function bar(real[2] xy) {}
+  function baz(uint32 x, bool y) returns (bool r) { r = x > 32 || y; }
+  function sam(bytes name, bool z, uint[] data) {}
+}
+
+*/
+const address = " "; // ETHEREUM contract
 const ABI = [
-// ERROR - CLARIFYING THIS
+
 ];
 
 const contract = new ethers.Contract(address, ABI, provider);
